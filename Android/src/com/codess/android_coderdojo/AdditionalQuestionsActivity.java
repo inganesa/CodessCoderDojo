705c01910fe579e71630341e5cff047f6cf3cffa
Ashley <eacortez2@pipeline.sbcc.edu>
2014-10-11 17:46:23 -0700

    add some more static connections...now Integration

--- Android/src/com/codess/android_coderdojo/AdditionalQuestionsActivity.java
+++ Android/src/com/codess/android_coderdojo/AdditionalQuestionsActivity.java
@@ -1,15 +1,40 @@
 package com.codess.android_coderdojo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AdditionalQuestionsActivity extends Activity {
 
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-			super.onCreate(savedInstanceState);
-			setContentView(R.layout.additional_questions);
-	}
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.additional_questions);
 
+	}
+//	
+//	public void onRadioButtonClicked(View view) {
+//	    // Is the button now checked?
+//	    boolean checked = ((RadioButton) view).isChecked();
+//	    
+//	    // Check which radio button was clicked
+//	    switch(view.getId()) {
+//	        case R.id.radioButton1:
+//	            if (checked)
+//	                // Pirates are the best
+//	            break;
+//	        case R.id.radioButton2:
+//	            if (checked)
+//	                // Ninjas rule
+//	            break;
+//	    }
+//	}
+
+	public void gotoPaired(View v) {
+
+		Intent intent = new Intent(this, PairedActivity.class);
+		startActivity(intent);
+
+	}
 }
--- Android/src/com/codess/android_coderdojo/PairedActivity.java
+++ Android/src/com/codess/android_coderdojo/PairedActivity.java
@@ -1,7 +1,9 @@
 package com.codess.android_coderdojo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class PairedActivity extends Activity {
 
@@ -9,6 +11,12 @@ public class PairedActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.paired);
+	}
+	
+	
+	public void gotoCollaborator(View v){
+		Intent intent = new Intent(this, CollaboratorActivity.class);
+		startActivity(intent);
 	}
 
 }
