70ce5deabcbe60fec86bf511d6372237cbbcc83e
Ashley <eacortez2@pipeline.sbcc.edu>
2014-10-11 18:10:46 -0700

    again...

--- Android/gen/com/codess/android_coderdojo/R.java
+++ Android/gen/com/codess/android_coderdojo/R.java
@@ -23,12 +23,15 @@ public final class R {
     }
     public static final class drawable {
         public static final int ashley_profile=0x7f020000;
-        public static final int buttonshape_blue=0x7f020001;
-        public static final int coder_dojosplash=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int logo_coder_dojo=0x7f020004;
-        public static final int profile_pic_default=0x7f020005;
-        public static final int rachel_profile=0x7f020006;
+        public static final int brackets_blue=0x7f020001;
+        public static final int brackets_green=0x7f020002;
+        public static final int brackets_logo=0x7f020003;
+        public static final int buttonshape_blue=0x7f020004;
+        public static final int coder_dojosplash=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
+        public static final int logo_coder_dojo=0x7f020007;
+        public static final int profile_pic_default=0x7f020008;
+        public static final int rachel_profile=0x7f020009;
     }
     public static final class id {
         public static final int action_settings=0x7f08001e;
